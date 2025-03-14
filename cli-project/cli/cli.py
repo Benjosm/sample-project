cba70e6a0a687f82d9722d168ecf09195938f7bc
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-03-14 14:37:38 -0500

    Implement file commands

--- cli-project/cli/cli.py
+++ cli-project/cli/cli.py
@@ -2,12 +2,15 @@
 import click
 from cli.commands.exec_command import exec_command
 
-@click.group()
+@click.group(name="cli")
 def cli():
     """A simple CLI tool."""
     pass
 
 cli.add_command(exec_command)
+
+from cli.commands.file_commands import file
+cli.add_command(file)
 
 if __name__ == '__main__':
     cli()
